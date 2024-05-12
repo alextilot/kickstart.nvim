6c906ba2ffeef64e3aeb4067bd031f586513c3fe
alextilot <alextilot@gmail.com>
2024-05-12 16:25:33 -0500

    feat: moved tmux to own module

--- lua/custom/plugins/nvim-tmux-navigation.lua
+++ lua/custom/plugins/nvim-tmux-navigation.lua
@@ -1,9 +1,5 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
+-- Configure nvim & tmux pane navigation.
 return {
-  -- Configure nvim & tmux pane navigation.
   {
     'alexghergh/nvim-tmux-navigation',
     config = function()

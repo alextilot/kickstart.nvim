b098c32e68aab18caee31051c836f75e1863f214
alextilot <alextilot@gmail.com>
2024-08-08 08:44:20 -0500

    feat: use terminal gui colors

--- init.lua
+++ init.lua
@@ -92,6 +92,7 @@ vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
+vim.o.termguicolors = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`

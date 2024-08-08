c65c542b4e15117d19b26f27f78a21f1c9941349
alextilot <alextilot@gmail.com>
2024-08-08 08:21:06 -0500

    feat: custom plugin diffview

---
+++ lua/custom/plugins/diffview.lua
@@ -0,0 +1,13 @@
+-- Configure diff view for git.
+return {
+  {
+    'sindrets/diffview.nvim',
+  },
+  vim.keymap.set('n', '<leader><leader>v', function()
+    if next(require('diffview.lib').views) == nil then
+      vim.cmd 'DiffviewOpen'
+    else
+      vim.cmd 'DiffviewClose'
+    end
+  end),
+}

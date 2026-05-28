31a1db13269a12413e333520a01a61b6e7a81761
Alex Tilot <alextilot@gmail.com>
2026-05-28 15:21:15 -0500

    fixup

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,9 +1,11 @@
 vim.pack.add { 'https://github.com/sindrets/diffview.nvim' }
 
+require('diffview').setup()
+
 vim.keymap.set('n', '<leader><leader>v', function()
   if next(require('diffview.lib').views) == nil then
-    vim.cmd 'DiffviewOpen'
+    vim.cmd.DiffviewOpen()
   else
-    vim.cmd 'DiffviewClose'
+    vim.cmd.DiffviewClose()
   end
 end, { desc = 'Toggle [V]iew diff' })
